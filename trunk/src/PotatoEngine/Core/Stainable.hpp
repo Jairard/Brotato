b608b14ef9585899bdf81e0cd2dbe803b77a7943
Jairard@Mac <gaetan.champarnaud@gmail.com>
2015-08-29 15:52:08 +0200

    Adding copy constructor and == operators for Stainable

--- trunk/src/PotatoEngine/Core/Stainable.hpp
+++ trunk/src/PotatoEngine/Core/Stainable.hpp
@@ -12,7 +12,12 @@ class Stainable
 			m_object(),
 			m_isRotten(false)
 		{}
-		
+
+		Stainable(const Stainable<T>& other):
+			m_object(other.const_object()),
+			m_isRotten(true)
+		{}
+
 		void soil(void)                   { m_isRotten = true; }
 		void wash(void)                   { m_isRotten = false; }
 		bool isRotten() const             { return m_isRotten; }
@@ -22,21 +27,31 @@ class Stainable
 			m_isRotten = true;
 			return m_object;
 		}
-		
+
 		Stainable<T>& operator=(const Stainable<T>& other)
 		{
 			m_object = other.m_object;
 			m_isRotten = true;
 			return *this;
 		}
-		
+
 		Stainable<T>& operator=(const T& other)
 		{
 			m_object = other;
 			m_isRotten = true;
 			return *this;
 		}
-		
+
+		bool operator==(const Stainable<T>& other)
+		{
+			return const_object() == other.const_object();
+		}
+
+		bool operator==(const T& other)
+		{
+			return const_object() == other;
+		}
+
 	private:
 		T m_object;
 		bool m_isRotten;
